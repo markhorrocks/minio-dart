5f080d62f3ab073b7ac23874fc19d51e8e2a09f4
Mark Horrocks <markdavidhorrocks@gmail.com>
2024-08-25 10:36:00 +0930

    Use user supplied s3 endpoint

--- lib/src/minio_s3.dart
+++ lib/src/minio_s3.dart
@@ -1,22 +1,7 @@
-const awsS3Endpoint = {
-  'us-east-1': 's3.amazonaws.com',
-  'us-east-2': 's3-us-east-2.amazonaws.com',
-  'us-west-1': 's3-us-west-1.amazonaws.com',
-  'us-west-2': 's3-us-west-2.amazonaws.com',
-  'ca-central-1': 's3.ca-central-1.amazonaws.com',
-  'eu-west-1': 's3-eu-west-1.amazonaws.com',
-  'eu-west-2': 's3-eu-west-2.amazonaws.com',
-  'sa-east-1': 's3-sa-east-1.amazonaws.com',
-  'eu-central-1': 's3-eu-central-1.amazonaws.com',
-  'ap-south-1': 's3-ap-south-1.amazonaws.com',
-  'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
-  'ap-southeast-2': 's3-ap-southeast-2.amazonaws.com',
-  'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com',
-  'cn-north-1': 's3.cn-north-1.amazonaws.com.cn'
-};
+// minio_s3.dart
 
-// getS3Endpoint get relevant endpoint for the region.
-String getS3Endpoint(String region) {
-  final endpoint = awsS3Endpoint[region];
-  return endpoint ?? 's3.amazonaws.com';
+// getS3Endpoint returns the user-supplied endpoint directly.
+String getS3Endpoint(String userSuppliedEndpoint) {
+  // Validate or modify the user-supplied endpoint if necessary
+  return userSuppliedEndpoint;
 }
